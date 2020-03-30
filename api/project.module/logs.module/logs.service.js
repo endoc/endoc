263742d95f1b2883dc64d45fcf18a8f5cf92a332
Agon G <agonxgashi@gmail.com>
2020-03-31 00:34:51 +0200

    console.logs

--- api/project.module/logs.module/logs.service.js
+++ api/project.module/logs.module/logs.service.js
@@ -5,9 +5,7 @@ module.exports = {
   list: async function (req, res) {
     try {
       const _projectId = req.params.project_id
-      console.log(_projectId)
       const _data = await Logs.find({ ProjectId: _projectId }).sort('CreateDate').exec()
-      console.log(_data)
       res.status(200).send(new ReturnObj(true, 'MSG_SUCCESS', 200, _data))
     } catch (error) {
       res.status(500).send(new ReturnObj(false, 'ERR_SOMETHING_WENT_WRONG', 500, null))
@@ -28,10 +26,8 @@ module.exports = {
     try {
       const _log = new Logs(req.body)
       _log.ProjectId = req.headers['authorization']
-      console.log(_log)
       const _data = await _log.save()
-      console.log(req.headers['authorization'])
-      res.status(200).send(new ReturnObj(true, 'MSG_LOG_SAVED', 200, null))
+      res.status(200).send(new ReturnObj(true, 'MSG_LOG_SAVED', 200, _data))
     } catch (error) {
       res.status(500).send(new ReturnObj(false, 'MSG_LOG_NOT_SAVED', 500, null))
     }
